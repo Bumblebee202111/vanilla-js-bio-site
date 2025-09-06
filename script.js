0be585b8eeb638333d6cf9eb96f7ad9ebf8b2a1f
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2025-09-06 17:29:26 +0800

    Add contact form with thank you message

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const bodyElement = document.querySelector('body');
 themeToggleButton.addEventListener('click', () => {
     bodyElement.classList.toggle('dark-mode');
 })
+
+const contactForm = document.querySelector('#contact-form');
+const thankYouMessage = document.querySelector('#thank-you-message');
+
+if (contactForm){
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        contactForm.style.display='none';
+
+        thankYouMessage.classList.remove('hidden');
+    })
+}
